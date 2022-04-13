b6c5bfc1b42c225d3a869ba96b6b4bca47260276
Maksym Shemet <max.shemet@gmail.com>
2022-04-13 16:50:19 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Pet from './Pet';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Adopt me!</h1>
+
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockateil" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
