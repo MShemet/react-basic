b54d951f7da14595b17fddbc754dc5a28e005ff4
Maksym Shemet <max.shemet@gmail.com>
2022-04-20 15:16:15 +0300

    add ssr

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { StrictMode, useState } from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { StrictMode, useState, lazy, Suspense } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ThemeContext from './ThemeContext';
-import SearchParams from './SearchParams';
 import Details from './Details';
+import SearchParams from './SearchParams';
+
+// const Details = lazy(() => import('./Details')); // lazy not support for ssr
+// const SearchParams = lazy(() => import('./SearchParams')); // lazy not support for ssr
 
 const App = () => {
   const theme = useState('darkBlue');
 
   return (
     <StrictMode>
+      {/* <Suspense fallback={<h2>loading, be patient</h2>}> */}
       <ThemeContext.Provider value={theme}>
         <div
           className="p-0 m-0"
@@ -18,22 +23,23 @@ const App = () => {
               'url(http://pets-images.dev-apis.com/pets/wallpaperB.jpg)',
           }}
         >
-          <BrowserRouter>
-            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
-              <Link to="/" className="text-6xl text-white hover:text-gray-200">
-                Adopt me!
-              </Link>
-            </header>
+          {/* <BrowserRouter> */}
+          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-purple-400 via-pink-500 to-red-500">
+            <Link to="/" className="text-6xl text-white hover:text-gray-200">
+              Adopt me!
+            </Link>
+          </header>
 
-            <Routes>
-              <Route path="/details/:id" element={<Details />} />
-              <Route path="/" element={<SearchParams />} />
-            </Routes>
-          </BrowserRouter>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+          {/* </BrowserRouter> */}
         </div>
       </ThemeContext.Provider>
+      {/* </Suspense> */}
     </StrictMode>
   );
 };
 
-render(<App />, document.getElementById('root'));
+export default App;
